5c2bbf488eb82391b4be8bff2e8d74d7dc18971b
Itztli Herzchen <166841132+Herzchens@users.noreply.github.com>
2026-06-24 14:13:20 +0700

    fix(source): fix ffmpeg user agent argument positioning

--- src/audio/source.rs
+++ src/audio/source.rs
@@ -726,17 +726,17 @@ pub async fn create_ffmpeg_stream_input(
         || stream.url.contains("youtu.be");
     let is_soundcloud = stream.client_kind == "SOUNDCLOUD" || stream.url.contains("soundcloud");
 
-    let mut headers = String::new();
-
     if is_youtube {
         let ua = if !stream.user_agent.is_empty() {
             &stream.user_agent
         } else {
             "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0 Safari/537.36"
         };
+        args.push("-user_agent".to_owned());
         args.push(ua.to_owned());
     } else {
-        headers.push_str("User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0 Safari/537.36\r\n");
+        args.push("-user_agent".to_owned());
+        args.push("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0 Safari/537.36".to_owned());
     }
 
     let is_web_youtube_client = stream.client_kind.is_empty()
